7f08e98549a429ab452723ef8a7f15ca662b0157
Ben Clifford <benc@hawaga.org.uk>
2021-10-29 12:36:21 +0000

    Remove dead code from f.py

--- f.py
+++ f.py
@@ -1,12 +1,4 @@
 
-
-
-def fib(myself, n):
-  if n == 0 or n == 1:
-    return 1
-  else:
-    return myself(myself, n-1) + myself(myself, n-2)
-
 for n in range(0,7):
   f = lambda myself, n: 1 if n==0 or n==1 else myself(myself, n-1) + myself(myself, n-2)
   print(f(f, n))
